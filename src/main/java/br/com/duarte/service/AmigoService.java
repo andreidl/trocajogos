9cd3fbe94d7c4f2e323e81bdd1bcb0e5aacabbaa
andrei.lima@imexperts.com.br <andrei.lima@imexperts.com.br>
2019-06-06 09:13:24 -0300

    projeto eclipse

--- src/main/java/br/com/duarte/service/AmigoService.java
+++ src/main/java/br/com/duarte/service/AmigoService.java
@@ -1,10 +1,12 @@
 package br.com.duarte.service;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import br.com.duarte.model.Amigo;
-import br.com.duarte.repository.JogoRepository;
+import br.com.duarte.repository.AmigoRepository;
 
 @Service
 public class AmigoService {
